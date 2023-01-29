7b84f82944069ecb2ce771e66a00acb3b5d9637b
Donovan Wright <donwwright42@gmail.com>
2023-01-28 17:59:30 -0700

    adding start / stop messages

--- src/main.py
+++ src/main.py
@@ -1,13 +1,21 @@
 # Libraries
 import os
+import atexit
 from dotenv import load_dotenv
-from classes.reddit import Reddit
-from classes.alert import Alerter
-from classes.offer import Offer
+from src.classes.reddit import Reddit
+from src.classes.alert import Alerter
+from src.classes.offer import Offer
 from src.shared_methods import valid_title
+
+
+# Methods
+def exit_handler():
+    alert.send("Program Stopped.")
+
 
 # Script Setup
 load_dotenv()
+atexit.register(exit_handler)
 
 # Classes
 reddit = Reddit(
@@ -22,16 +30,18 @@ alert = Alerter(
     token=os.getenv("TELEGRAM_TOKEN")
 )
 
+
+alert.send("Program Started.")
+
 # for submission in reddit.api.subreddit("all").stream.submissions(skip_existing=True):
 #     if valid_title(submission.title):
 #         test = Offer(submission)
-#         print(test)
-#         print()
+#         if test.on_alert_list:
+#             alert.send(test.format_for_telegram())
+
 
 for submission in reddit.api.subreddit("hardwareswap").new(limit=50):
     if valid_title(submission.title):
         test = Offer(submission)
-        print(test)
-        print()
         if test.on_alert_list:
             alert.send(test.format_for_telegram())
